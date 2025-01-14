059868b654aee1fae2c57854e64af0f13c87409c
marslf <mars.lf21@gmail.com>
2025-01-14 15:46:09 -0500

    working on task 5

--- task5/js/script.js
+++ task5/js/script.js
@@ -1,10 +1,28 @@
 "use strict";
 
+let counter = 0;
+
+let square = {
+    x: 10,
+    y: 10,
+    w: 20,
+    h: 20,
+    color: "orange",
+}
+
 function setup() {
     console.log("go")
-
+    createCanvas(600, 600);
 }
 
 function draw() {
+    background(0);
+    displaySquare();
+}
 
+function displaySquare() {
+    push();
+    fill(square.color);
+    rect(square.x, square.y, square.w, square.h);
+    pop();
 }
