025a9d1061a589cc985bc2b124a1bf532bdca291
marslf <mars.lf21@gmail.com>
2025-01-17 22:46:09 -0500

    Hover + click function on the square

--- task5/js/script.js
+++ task5/js/script.js
@@ -5,9 +5,13 @@ let counter = 0;
 let square = {
     x: 10,
     y: 10,
-    w: 20,
-    h: 20,
-    color: "orange",
+    w: 30,
+    h: 30,
+    color: {
+        r: 255,
+        g: 138,
+        b: 0,
+    }
 }
 
 function setup() {
@@ -17,12 +21,30 @@ function setup() {
 
 function draw() {
     background(0);
+    
+    if (checkCollisionWithSquare()) {
+        fill(255, 180, 100);
+    } else {
+        fill(square.color.r, square.color.g, square.color.b);
+    }
     displaySquare();
 }
 
 function displaySquare() {
     push();
-    fill(square.color);
     rect(square.x, square.y, square.w, square.h);
     pop();
+}
+
+function checkCollisionWithSquare() {
+    return mouseX >= square.x &&
+        mouseX <= square.x + square.w &&
+        mouseY >= square.y &&
+        mouseY <= square.y + square.h;
+}
+
+function mousePressed() {
+    if (checkCollisionWithSquare()) {
+        counter++;
+    }
 }
