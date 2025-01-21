edb83711e9e5005175e9cc55ebc04f66798937cf
marslf <mars.lf21@gmail.com>
2025-01-21 14:25:20 -0500

    week 2 example

---
+++ exampleweek2/js/1_helloWorld.js
@@ -0,0 +1,12 @@
+document.write("<p>HELLO WORLD DYNAMIC</p>");
+console.log("hello world")
+
+//window.onload = function(){
+
+//}
+
+window.onload = setup //when the window loads it will do the setup function 
+
+function setup() {
+    console.log("hello");
+}
