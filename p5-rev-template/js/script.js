235100735cd25db316a0c0ee482a782e64cb678f
marslf <mars.lf21@gmail.com>
2025-01-21 13:49:46 -0500

    week 2 array review

--- p5-rev-template/js/script.js
+++ p5-rev-template/js/script.js
@@ -1,26 +1,9 @@
-/**task #6 */
-
 "use strict";
 
 function setup() {
     console.log("go")
-    createCanvas(500, 500);
-    textSize(28);
-    fill(255);
-    textAlign(CENTER, CENTER);
 }
 
 function draw() {
-    background(0);
-
-    text("test", width / 2, height / 2);
-
-    for (let i = 0; i < 10; i++) {
-        text(i, 50 + i * 20, 30);
-    }
-
-    for (let i = 15; i > 0; i--) {
-        text(i, 20, (16 - i) * 30);
-    }
 
 }
