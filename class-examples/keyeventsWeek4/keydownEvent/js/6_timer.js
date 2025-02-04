d8943a43fc2f50cb84e9f97689b63bc81b5aa819
marslf <mars.lf21@gmail.com>
2025-02-04 14:34:23 -0500

    week 4 in class examples + start task 3

---
+++ class-examples/keyeventsWeek4/keydownEvent/js/6_timer.js
@@ -0,0 +1,128 @@
+window.onload = function () {
+
+    //Reference for using the remainder for MOD: https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/what-is-modular-arithmetic
+
+    // let gridArray = [];
+    // for (let i = 0; i < 24; i++) {
+    //     //for each x - make a column of changing y's
+    //     let gridCol = [];
+    //     for (let j = 0; j < 24; j++) {
+    //         //create a grid cell with a div
+    //         let parent = document.getElementById("parent");
+    //         let d = document.createElement("div");
+    //         d.classList.add("grid-cell");
+    //         parent.appendChild(d);
+
+    //         d.style.left = (i + 1) * 25 + "px";
+    //         d.style.top = (j + 1) * 25 + "px";
+    //         gridCol[j] = d;
+    //     }
+    //     //put each grid row into the grid array
+    //     gridArray[i] = gridCol;
+    // }
+    // console.log(gridArray);
+
+    // let shades = [ //array of shades
+    //     "#7fb3d5", //grey blue first
+    //     "#76d7c4",
+    //     "#f7dc6f",
+    //     "#eb984e",
+    //     "#cb4335",
+    //     "#8e44ad",
+    //     "#2e4053",
+    //     "#e5e7e9",
+    // ];
+
+    // //the divisor
+    // let num = 2;
+
+    // //ALTERNATING ROWS (j)
+    // //if the remainder is 0 = set a type of background and if the remainder is 1 = set different background
+    // // for (let i = 0; i < 24; i++) {
+    // //     for (let j = 0; j < 24; j++) {
+    // //         //check the j--> y value for color choice (all same ys will have the same color (a row))
+    // //         let d = gridArray[i][j];
+    // //         if (j % num === 0) {
+    // //             d.style.background = shades[0];
+    // //         } else if (j % num === 1) {
+    // //             d.style.background = shades[1];
+    // //         }
+    // //     }
+    // // }
+
+    // // //ALTERNATING COLUMN (i)
+    // // for (let i = 0; i < 24; i++) {
+    // //     for (let j = 0; j < 24; j++) {
+    // //         //check the i -> x value for color choice (all same xs will have the same color (a column))
+    // //         let d = gridArray[i][j];
+    // //         if (i % num === 0) {
+    // //             d.style.background = shades[0];
+    // //         } else if (i % num === 1) {
+    // //             d.style.background = shades[1];
+    // //         }
+    // //     }
+    // // }
+
+    // //setInterval(animate_cells_mod_rows, 1000); //every second
+    // setInterval(animate_cells_mod_rows, 200); //shorter interval = faster 
+
+    // function animate_cells_mod_rows() {
+
+    //     //animate the rows 
+    //     //switch j to i if you want to animate the columns 
+    //     for (let i = 0; i < 24; i++) {
+    //         for (let j = 0; j < 24; j++) {
+    //             let d = gridArray[i][j]
+    //             //check the j--> y value for color choice (all same ys will have the same color (a row))
+    //             if (j % num === 0) {
+    //                 d.style.background = shades[0];
+    //             } else if (j % num === 1) {
+    //                 d.style.background = shades[1];
+    //             } else if (j % num === 2) {
+    //                 d.style.background = shades[2];
+    //             } else if (j % num === 3) {
+    //                 d.style.background = shades[3];
+    //             } else if (j % num === 4) {
+    //                 d.style.background = shades[4];
+    //             } else if (j % num === 5) {
+    //                 d.style.background = shades[5];
+    //             } else if (j % num === 6) {
+    //                 d.style.background = shades[6];
+    //             } else if (j % num === 7) {
+    //                 d.style.background = shades[7];
+    //             }
+    //         }
+    //     }
+
+
+
+    //     num += 1;
+    //     console.log(num);
+    //     if (num === 9) { num = 1 } //num increase by one and once it its 9 it goes back to 1
+    // }
+
+    //add stars ever 500ms
+
+
+    // window.setInterval(addOtherText, 500);
+    // function addOtherText() {
+    //     let sp = document.createElement("span");
+    //     sp.textContent = " ***-*** ";
+    //     sp.classList.add("appearInStarText");
+    //     document.getElementById("parent").appendChild(sp);
+    // }
+
+    let ref = window.setInterval(addOtherText, 500); //ref = setInterval varriable for this specific one (useful when you have many setIntervals)
+    let counter = 0;
+    function addOtherText() {
+        let sp = document.createElement("span");
+        sp.textContent = " ***-*** ";
+        sp.classList.add("appearInStarText");
+        document.getElementById("parent").appendChild(sp);
+        counter++;
+        if (counter === 10) { //when counter runs out it stops 
+            clearInterval(ref);
+        }
+    }
+
+};
--- class-examples/keyeventsWeek4/keydownEvent/js/keyEx.js
+++ class-examples/keyeventsWeek4/keydownEvent/js/keyEx.js
@@ -1,3 +1,84 @@
+let speedX = 5;
+
 window.onload = function () {
     console.log("keys");
+
+    //Timeout = 2 seconds after window is loaded = event triggered (2000 milliseconds)
+    window.setTimeout(addTimeoutText, 2000);
+    function addTimeoutText() {
+        let parent = document.getElementById("parent");
+        parent.innerHTML += "<p> NEW TEXT TO APPEAR </p>";
+    }
+
+//Interval = every 2 seconds, add new text 
+    window.setInterval(addTextRecur, 2000);
+
+    function addTextRecur() {
+        let parent = document.getElementById("parent");
+        parent.innerHTML += "<p> NEW TEXT TO APPEAR RECUR </p>";
+    }
+
+    window.addEventListener("keydown", keyHandler)
+
+    function keyHandler(event) {
+        console.log(event); //use it to see what happens
+
+        //access the element with the idea + change the text content
+        document.querySelector("#textContainer").textContent =
+            document.querySelector("#textContainer").textContent += event.code + ' '; //output the keys 
+
+        document.querySelector("#textContainer").textContent =
+            document.querySelector("#textContainer").textContent += event.code + ' ';
+
+        if (event.key === "ArrowRight") { //move boxA if arrow keys are pressed
+            //parseInt = extract the string into a number = parse a value as a string and return the first integer in a specified radix
+            document.getElementById("boxA").style.left = parseInt(document.getElementById("boxA").style.left) + speedX + "px";
+        }
+        else if (event.key === "ArrowLeft") {
+
+            document.getElementById("boxA").style.left = parseInt(document.getElementById("boxA").style.left) - speedX + "px";
+
+
+        }
+
+        //     //turn boxB orange
+        // else if (event.code === "Space") {
+        //     document.getElementById("boxB").style.background = "orange";
+
+        // }
+
+        //toggle on and off orange BoxB
+        //access custom boolean attribute which we turn on or off to dictate color (custom bool already in HTML)
+        else if (event.code === "Space") {
+            let bool = document.getElementById("boxB").getAttribute("custom-bool");
+            if (bool === "off") {
+                document.getElementById("boxB").style.background = "orange";
+                document.getElementById("boxB").setAttribute("custom-bool", "on");
+            } else {
+                document.getElementById("boxB").style.background = "rgb(112, 184, 226)";
+                document.getElementById("boxB").setAttribute("custom-bool", "off");
+            }
+        }
+        // turn boxA pink when Shift pressed
+        else if (event.key === "Shift") {
+            document.getElementById("boxA").style.background = "rgb(226, 112, 177)";
+        }
+
+    }
+    // when shift is let go =  boxA goes back to blue 
+    window.addEventListener("keyup", function (event) {
+        console.log("keyup")
+        //2: change color when space is down
+        if (event.key === "Shift") {
+            document.getElementById("boxA").style.background = "rgb(112, 184, 226)";
+
+        }
+        else {
+            console.log(`key up not shift:`)
+            console.log(event)
+        }
+
+    })
+
 }
+
