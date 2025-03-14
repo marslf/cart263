97f44babc30b4c4e70b7d543e21d6ae28b195a0c
marslf <mars.lf21@gmail.com>
2025-03-13 23:43:23 -0400

    Task 6 Part A working

--- tasks/task-6/js/main.js
+++ tasks/task-6/js/main.js
@@ -4,15 +4,36 @@ function run() {
   document.querySelector("#stepOneButton").addEventListener("click", fetchText);
 
 
- /****** PART A:: FETCH */  
- async function fetchText() {
+  /****** PART A:: FETCH */
+  async function fetchText() {
     console.log("in fetch");
-    let raw_rainbow_text = "";
+
     try {
-     
-      document.querySelector("#resetButton").addEventListener("click", resetPoem);
+      // Fetch the text file
+      const response = await fetch("files/rainbow.txt");
+
+      // Check fetch was successful
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      // Convert response to text and store in raw_rainbow_text
+      let raw_rainbow_text = await response.text();
+
+      // Hide the button
+      document.querySelector("#stepOneButton").style.display = "none";
+
+      // Display the div with id inputDiv
+      document.querySelector("#inputDiv").style.display = "block";
+
+      // Display fetched text
+      document.querySelector("#rainbow_text").innerText = raw_rainbow_text;
+
+      // Run runPartB once everything else works
       runPartB(raw_rainbow_text);
-    } catch (e) {}
+    } catch (error) {
+      console.error("Error fetching rainbow.txt:", error);
+    }
   }
 
   /****** PART B:: TEXT PROCESSING  */
@@ -21,7 +42,7 @@ function run() {
       .querySelector("#produce-poem")
       .addEventListener("click", producePoem);
 
-   /* FILL IN HERE */
+    /* FILL IN HERE */
     function producePoem() {
       console.log(originalRainBowText)
       //SR
@@ -35,21 +56,21 @@ function run() {
   function runPartC(rainbow_words, seed_phrase_array) {
     console.log(rainbow_words);
     console.log(seed_phrase_array);
- 
+
     //to next stage
     runPartD(poem_sentence);
   }
 
-  
-   /****** PART D:: VISUALIZE  */
-  function runPartD(new_sentence){
+
+  /****** PART D:: VISUALIZE  */
+  function runPartD(new_sentence) {
 
   }
 
   /****** PART E:: RESET  */
   function resetPoem() {
-  /*** TO FILL IN */
-  
+    /*** TO FILL IN */
+
   }
 } //window onload
 
