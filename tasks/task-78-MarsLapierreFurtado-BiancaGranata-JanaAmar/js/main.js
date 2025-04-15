56b7bc0c0a0bd32efc2ba61a0df90d1e898b89bd
marslf <mars.lf21@gmail.com>
2025-04-14 22:04:20 -0400

    load data from iris.json

--- tasks/task-78-MarsLapierreFurtado-BiancaGranata-JanaAmar/js/main.js
+++ tasks/task-78-MarsLapierreFurtado-BiancaGranata-JanaAmar/js/main.js
@@ -1,4 +1,11 @@
-window.onload = async function(){
+window.onload = async function () {
     console.log("task 7-8");
+    try {
+        const response = await fetch("data/iris.json");
+        const irisData = await response.json();
+        console.log("Iris data loaded:", irisData);
 
+    } catch (error) {
+        console.error("Error loading iris data:", error);
+    }
 }
