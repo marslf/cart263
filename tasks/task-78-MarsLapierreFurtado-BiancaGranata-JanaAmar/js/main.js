e7dfa7f4c787c5b6f23ae6e5b6e21ca2819e3b6a
marslf <mars.lf21@gmail.com>
2025-04-14 22:08:14 -0400

    map() new array

--- tasks/task-78-MarsLapierreFurtado-BiancaGranata-JanaAmar/js/main.js
+++ tasks/task-78-MarsLapierreFurtado-BiancaGranata-JanaAmar/js/main.js
@@ -1,5 +1,17 @@
+let possibleColor = ["#5d3fd3", "#a73fd3", "#d33fb5", "#d35d3f", "#d3a73f"];
+
+const irisesWithColors = irisData.map(iris => {
+    //2- Map() = Randomly select color from array
+    const randomIndex = Math.floor(Math.random() * possibleColor.length);
+    return { ...iris, color: possibleColor[randomIndex] };
+});
+
+console.log("Irises with Colors:", irisesWithColors);
+
 window.onload = async function () {
     console.log("task 7-8");
+
+    //1- Load data from iris.json
     try {
         const response = await fetch("data/iris.json");
         const irisData = await response.json();
